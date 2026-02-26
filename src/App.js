0b0e1b271881e457417530745f89d972c9b20b8f
aj-pzc <aldoempoder@gmail.com>
2026-02-25 22:37:13 -0600

    hook

--- src/App.js
+++ src/App.js
@@ -1,42 +1,13 @@
 import Header from "./components/Header/index";
-import SongInfo from "./components/Songs/index";
+import SongList from "./components/SongList/index";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header appName="MediaPlayerApp" />
+      <Header appName="MediaPlayerApp" />  
+      <SongList />   
 
-    <div className="songList">
-       <SongInfo
-        title="Die For You" 
-        artist="The Weeknd" 
-        duration="4:20" 
-      />
-      <SongInfo
-        title="Blinding Lights" 
-        artist="The Weeknd" 
-        duration="3:20" 
-      />
-      <SongInfo
-        title="São Paulo" 
-        artist="The Weeknd" 
-        duration="5:02" 
-      />
-      <SongInfo
-        title="Timeless"
-        artist="The Weeknd" 
-        duration="4:16" 
-      />
-
-      <SongInfo
-        title="The Hills"
-        artist="The Weeknd" 
-        duration="4:02" 
-      />
-
-
-    </div>
     </div>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const Header = ({appName}) => {
     return (
         <header className="header">
